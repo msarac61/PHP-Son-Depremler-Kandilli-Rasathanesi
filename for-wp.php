8d3e383873f39058557d29f60f0db2de1de551c6
msarac61 <metin_@hotmail.com.tr>
2023-04-18 03:18:06 +0300

    beginning

---
+++ for-wp.php
@@ -0,0 +1,27 @@
+<?php
+
+function depremler() {
+    $veri = simplexml_load_file("http://udim.koeri.boun.edu.tr/zeqmap/xmlt/son24saat.xml");
+    $depremlerArray = array();
+    foreach ($veri as $eq) {
+        $depremlerArray[] = array(
+            "date" => date('d.m.Y H:i:s', strtotime((string)$eq->tarih)),
+            "latitude" => $eq->attributes()->lat,
+            "longitude" => $eq->attributes()->lng,
+            "depth" => $eq->attributes()->Depth,
+            "magnitude" => $eq->attributes()->mag,
+            "region" => $eq->attributes()->lokasyon
+        );
+    }
+
+    echo '<ul>';
+    foreach ($depremlerArray as $eq) :
+        echo '<li>';
+        echo $eq['date'] . '' . $eq['magnitude'];
+        echo $eq['region'] . '' . $eq['depth'] . 'km';
+        echo '</li>';
+    endforeach;
+    echo '</ul>';
+}
+
+add_shortcode('depremler', 'depremler');
